9f04fd55fd904a263b8059a22179eec1f3875089
online_gh <online_gh@hotmail.com>
2019-04-22 07:00:44 -0400

    Blog App Model Created, Migrated and Running

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
+# -FLOW TO CREATING MODELS-
+# 1.Create A Blog Models
+# 2.Add the Blog App to Settings
+# 3.Create Migration
+# 3.Migrate
+# 4.Add to the admin
+
+
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=30)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
